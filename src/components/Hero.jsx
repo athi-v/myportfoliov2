77a897a8c83854f65d225091eae668fdade087dc
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-05-13 08:39:44 +0200

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import { Link } from "react-scroll";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+const navigate = useNavigate()
+  const handleSubmit = () => {
+
+    try {
+      navigate('/projects')
+  }
+    catch(error)
+    {
+      console.log(error)
+    }
+
+  }
   return (
     <div name="hero" className="w-full h-screen bg-[#f1f5f9]">
       {/* Container */}
@@ -18,16 +30,16 @@ const Hero = () => {
           Welcome to my personal portfolio
         </p>
 
-        {/* <div>
-          <a href="/projects">
-            <button className="text-[#0f172a] bg-teal-600 border-teal-600 group border-2 px-6 py-3 rounded flex items-center hover:bg-teal-900 hover:border-teal-900">
+        <div>
+          {/* <Link to="/projects"> */}
+            <button onClick={handleSubmit} className="text-[#0f172a] bg-teal-600 border-teal-600 group border-2 px-6 py-3 rounded flex items-center hover:bg-teal-900 hover:border-teal-900">
               See Projects
               <span className="group-hover:rotate-90 duration-500">
                 <HiArrowNarrowRight className="ml-3" />
               </span>
             </button>
-          </a>
-        </div> */}
+          {/* </Link> */}
+        </div>
       </div>
     </div>
   );
